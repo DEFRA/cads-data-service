d4c63231406a2f2e9c2b77ec11974e4796768790
andy-defra <andy.rose@esynergy.co.uk>
2026-02-23 12:42:25 +0000

    Fixing formatting errors

--- src/BuildingBlocks/Cads.Cds.BuildingBlocks.Infrastructure/Database/Services/PostgresStatusService.cs
+++ src/BuildingBlocks/Cads.Cds.BuildingBlocks.Infrastructure/Database/Services/PostgresStatusService.cs
@@ -3,16 +3,23 @@ using Cads.Cds.BuildingBlocks.Infrastructure.Database.Abstractions;
 namespace Cads.Cds.BuildingBlocks.Infrastructure.Database.Services;
 
 public class PostgresStatusService(HealthCheckDbContext healthCheckDbContext, HealthCheckReadOnlyDbContext healthCheckReadOnlyDbContext) : IPostgresStatusService
-{ 
+{
     public async Task<PostgresStatusServiceResult> CanConnect(CancellationToken cancellationToken = default)
     {
         var canConnectTask = healthCheckDbContext.Database.CanConnectAsync(cancellationToken);
         var readOnlyCanConnectTask = healthCheckReadOnlyDbContext.Database.CanConnectAsync(cancellationToken);
-    
+
         var results = await Task.WhenAll(canConnectTask, readOnlyCanConnectTask);
         var result = new PostgresStatusServiceResult { CanConnect = results[0] && results[1] };
-        if(!results[0]) result.ErrorMessage = "Could not connect to default Postgres database.";
-        if(!results[1]) result.ErrorMessage += "Could not connect to readonly Postgres database.";
+        if (!results[0])
+        {
+            result.ErrorMessage = "Could not connect to default Postgres database.";
+        }
+
+        if (!results[1])
+        {
+            result.ErrorMessage += "Could not connect to readonly Postgres database.";
+        }
         return result;
     }
 }
