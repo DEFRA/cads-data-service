6a3c91980382fd7371f302aeeec5124891399ce5
Thomas Harrison <taharrison@gmail.com>
2026-02-24 15:01:37 +0000

    Feature/ulitp 4522 - add integration test fixture (#16)
    * add postgres healthcheck integration test
    
    * add aws localstack to local test fixture
    
    * move classes & minor refactor
    
    * fix test
    
    * fix tests and msbuild warning
    
    * remove todos
    
    * make connections required config, fix test
    
    * set up readonly connection for integration tests
    
    * add test containers for each integration project and update pr check

---
+++ src/BuildingBlocks/Cads.Cds.BuildingBlocks.Testing.Support/TestFixtures/PostgresFixture.cs
@@ -0,0 +1,83 @@
+using Npgsql;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace Cads.Cds.BuildingBlocks.Testing.Support.TestFixtures;
+
+public class PostgresFixture : IAsyncLifetime
+{
+    public PostgreSqlContainer? Container { get; private set; }
+
+    public string NetworkName { get; } = "integration-test-network";
+    private const string CadsDatabaseName = "cads";
+    private const string PostgresUserName = "postgres";
+    private const string PostgresPassword = "password";
+    private const string PostgresReadUser = "readonlyuser";
+    private const string PostgresReadPassword = "readonly";
+
+    public static string ConnectionString =>
+        $"Host=postgres;Port=5432;Database={CadsDatabaseName};Username={PostgresUserName};Password={PostgresPassword}";
+
+    public static string ReadConnectionString =>
+        $"Host=postgres;Port=5432;Database={CadsDatabaseName};Username={PostgresReadUser};Password={PostgresReadPassword}";
+
+    private string InitialisationConnectionString =>
+        $"Host=127.0.0.1;Port=5432;Database=postgres;Username={PostgresUserName};Password={PostgresPassword}";
+
+    public async ValueTask InitializeAsync()
+    {
+        DockerNetworkHelper.EnsureNetworkExists(NetworkName);
+
+        Container = new PostgreSqlBuilder("postgres:16.6")
+            .WithName("postgres")
+            .WithPortBinding(5432, 5432)
+            .WithEnvironment("POSTGRES_USER", PostgresUserName)
+            .WithEnvironment("POSTGRES_PASSWORD", PostgresPassword)
+            .WithNetwork(NetworkName)
+            .WithNetworkAliases("postgres")
+            .Build();
+
+        await Container.StartAsync();
+        var conn = Container.GetConnectionString();
+
+        InitialiseDatabaseSchema();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await Container!.DisposeAsync();
+    }
+
+    private void InitialiseDatabaseSchema()
+    {
+        using var connection = new NpgsqlConnection(InitialisationConnectionString);
+        var createDbCommand = new NpgsqlCommand(
+            @$"
+            CREATE DATABASE {CadsDatabaseName}
+                WITH OWNER = postgres
+                ENCODING = 'UTF8'
+                CONNECTION LIMIT = -1",
+            connection);
+
+        var createROUserCommand = new NpgsqlCommand(
+            @$"
+            CREATE ROLE {PostgresReadUser} WITH LOGIN PASSWORD '{PostgresReadPassword}';
+            GRANT CONNECT ON DATABASE {CadsDatabaseName} TO {PostgresReadUser};
+            GRANT USAGE ON SCHEMA public TO {PostgresReadUser};
+            GRANT SELECT ON ALL TABLES IN SCHEMA public TO {PostgresReadUser};
+            REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO {PostgresReadUser};",
+            connection);
+
+        connection.Open();
+        createDbCommand.ExecuteNonQuery();
+        createROUserCommand.ExecuteNonQuery();
+        connection.Close();
+
+        ApplyDatabaseMigrations();
+    }
+
+    private void ApplyDatabaseMigrations()
+    {
+    }
+}
