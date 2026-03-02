50a94399f92a9071a7d0e0061365b4f13bc25515
andy-defra <andy.rose@esynergy.co.uk>
2026-03-02 15:48:55 +0000

    Trivial tidy up

--- src/Cads.Cds/Setup/WebApplicationExtensions.cs
+++ src/Cads.Cds/Setup/WebApplicationExtensions.cs
@@ -34,6 +34,7 @@ public static class WebApplicationExtensions
         app.UseMiddleware<ExceptionHandlingMiddleware>();
         app.UseCorrelationId();
         app.UseMiddleware<ApiResponseMiddleware>();
+
         app.UseAuthentication();
         app.UseAuthorization();
 
