304d55a9540ca5589677c91cf89031d2945a685b
andy-defra <andy.rose@esynergy.co.uk>
2026-03-11 14:06:35 +0000

    Fixing whitespace

--- tests/Cads.Cds.Tests.Integration/Endpoints/HealthcheckEndpointTests.cs
+++ tests/Cads.Cds.Tests.Integration/Endpoints/HealthcheckEndpointTests.cs
@@ -11,19 +11,19 @@ public class HealthcheckEndpointTests(ApiContainerFixture apiContainerFixture)
     {
         var response = await apiContainerFixture.HttpClient.GetAsync("health", TestContext.Current.CancellationToken);
         var responseBody = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
-    
+
         response.EnsureSuccessStatusCode();
         responseBody.Should().NotBeNullOrEmpty().And.Contain("\"status\": \"Healthy\"");
         responseBody.Should().NotContain("\"status\": \"Degraded\"");
         responseBody.Should().NotContain("\"status\": \"Unhealthy\"");
     }
-    
+
     [Fact]
     public async Task GivenValidHttpsHealthCheckRequest_ShouldSucceed()
     {
         var response = await apiContainerFixture.HttpsClient.GetAsync("health", TestContext.Current.CancellationToken);
         var responseBody = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
-    
+
         response.EnsureSuccessStatusCode();
         responseBody.Should().NotBeNullOrEmpty().And.Contain("\"status\": \"Healthy\"");
         responseBody.Should().NotContain("\"status\": \"Degraded\"");
