e3b5c554f4f465a238e696372e0b5871fc3ee5e4
Mark Gent <Mark.Gent@homesengland.gov.uk>
2026-03-12 13:43:22 +0000

    ULITP-4785: Ensure HTTPS runs with docker-compose.

--- src/Modules/Cads.Cds.Ingester/Cads.Cds.Ingester.Core/Configuration/ModuleConfigurationSection.cs
+++ src/Modules/Cads.Cds.Ingester/Cads.Cds.Ingester.Core/Configuration/ModuleConfigurationSection.cs
@@ -4,5 +4,5 @@ public static class ModuleConfigurationSection
 {
     public const string ModuleSectionName = "Modules:Ingester";
 
-    public static readonly string QueuesSectionName = $"{ModuleSectionName}:Storage";
+    public static readonly string QueuesSectionName = $"{ModuleSectionName}:Queues";
 }
