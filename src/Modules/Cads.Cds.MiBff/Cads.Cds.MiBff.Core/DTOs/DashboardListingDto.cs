7c38c59b92348702a9c8a3a353e808a5ca9af70a
Tom Harrison <taharrison@gmail.com>
2026-03-12 10:02:55 +0000

    fix json serialisation constraint

--- src/Modules/Cads.Cds.MiBff/Cads.Cds.MiBff.Core/DTOs/DashboardListingDto.cs
+++ src/Modules/Cads.Cds.MiBff/Cads.Cds.MiBff.Core/DTOs/DashboardListingDto.cs
@@ -1,10 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace Cads.Cds.MiBff.Core.DTOs;
 
 public class DashboardListingDto
 {
+    [JsonRequired]
     public required string ReportId { get; set; }
+    [JsonRequired]
     public required string ReportKey { get; set; }
+    [JsonRequired]
     public required string Title { get; set; }
+    [JsonRequired]
     public required string Description { get; set; }
     public bool IsActive { get; set; }
     public DateTime CreatedAt { get; set; }
