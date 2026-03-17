1b30a78f09f0f6f703efda36d35be2c1bff47841
David Baillie <david.baillie@esynergy.co.uk>
2026-03-17 07:59:40 +0000

    Updated AddRepositories to add repositories to service collection

--- src/BuildingBlocks/Cads.Cds.BuildingBlocks.Infrastructure/Repositories/Setup/ServiceCollectionExtensions.cs
+++ src/BuildingBlocks/Cads.Cds.BuildingBlocks.Infrastructure/Repositories/Setup/ServiceCollectionExtensions.cs
@@ -9,7 +9,16 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration config)
     {
         services.AddScoped<IMiEffectiveReportPermissionRepository, MiEffectiveReportPermissionRepository>();
-
+        services.AddScoped<IMiPermissionRepository, MiPermissionRepository>();
+        services.AddScoped<IMiReportGroupRepository, MiReportGroupRepository>();
+        services.AddScoped<IMiReportGroupMapRepository, MiReportGroupMapRepository>();
+        services.AddScoped<IMiReportRepository, MiReportRepository>();
+        services.AddScoped<IMiRoleReportPermissionRepository, MiRoleReportPermissionRepository>();
+        services.AddScoped<IMiRoleRepository, MiRoleRepository>();
+        services.AddScoped<IMiUserReportPermissionRepository, MiUserReportPermissionRepository>();
+        services.AddScoped<IMiUserRepository, MiUserRepository>();
+        services.AddScoped<IMiUserRoleRepository, MiUserRoleRepository>();
+        
         return services;
     }
 }
