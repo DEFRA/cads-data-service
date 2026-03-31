9d1f3941182e666549f6883cb79a52b366621a8f
andy-defra <andy.rose@esynergy.co.uk>
2026-03-31 15:18:10 +0100

    Adding integration test for new endpoint

--- tests/Modules/Cads.Cds.Ingester/Cads.Cds.Ingester.Tests.Integration/AnimalMovementsEndpoints/AnimalMovementsEnpointTests.cs
+++ tests/Modules/Cads.Cds.Ingester/Cads.Cds.Ingester.Tests.Integration/AnimalMovementsEndpoints/AnimalMovementsEnpointTests.cs
@@ -16,33 +16,11 @@ public class AnimalMovementsEnpointTests(ApiContainerFixture apiContainerFixture
     public async Task GivenValidAnimalMovements_WhenPostingToEndpoint_ShouldSucceed()
     {
         var region = Region.Wales;
-        var requestBody = new AnimalMovementsRequest
-        {
-            AnimalMovement = new AnimalMovement
-            {
-                CreatedBy = "WA",
-                DepartureRegion = "WA",
-                DestinationRegion = "WA",
-                MessageID = "123456789",
-                MovementTime = DateTime.UtcNow,
-                MovementID = "123456789",
-                MovementGroupID = "123456789",
-                MovementGroup = "TestGroup",
-                CrossBorderMovementId = "123456789",
-                MovementType = "TestMovementType",
-                ActionType = "TestActionType",
-                ActionStatus = "TestActionStatus",
-                SupplierType = "TestSupplierType",
-                AnimalType = "TestAnimalType",
-                MovementDetails = new MovementDetails()
-            }
-        };
-
-        var payload = JsonConvert.SerializeObject(requestBody);
         var endpoint = string.Format(TestEndpointConstants.AnimalMovementIngestionEndpoint, region);
-        var client = await apiContainerFixture.CreateAzureAdClientAsync();
-        
-        var result = await client.PostAsync(endpoint, new StringContent(payload, Encoding.UTF8, "application/json"), TestContext.Current.CancellationToken);
+        var client = apiContainerFixture.CreateBasicClient();
+
+        var result = await client.PostAsync(endpoint, new StringContent(payload, Encoding.UTF8, "application/json"),
+            TestContext.Current.CancellationToken);
         result.IsSuccessStatusCode.Should().BeTrue();
         var responseBody = await result.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
         var responseData = JsonConvert.DeserializeObject<IngestionDTO>(responseBody);
@@ -50,4 +28,85 @@ public class AnimalMovementsEnpointTests(ApiContainerFixture apiContainerFixture
         responseData.IngestionId.Should().Contain(region.ToString().ToLower());
         responseData.RecordCount.Should().Be(1);
     }
+
+    private readonly string payload = @"{
+ ""AnimalMovement"": {
+   ""CreatedBy"": ""SC"",
+   ""DepartureRegion"": ""SC"",
+   ""DestinationRegion"": ""SC"",
+   ""MessageID"": ""14567413"",
+   ""MovementTime"": ""2026-03-23T00:00:00Z"",
+   ""MovementID"": ""8308310"",
+   ""MovementGroupID"": ""2474189:31710"",
+   ""MovementGroup"": ""2474189"",
+   ""CrossBorderMovementId"": """",
+   ""MovementType"": ""01"",
+   ""ActionType"": ""New"",
+   ""ActionStatus"": ""Completed"",
+   ""SupplierType"": ""Receiver"",
+   ""AnimalType"": ""Sheep"",
+   ""MovementDetails"": {
+     ""DepartureDate"": ""2026-02-26"",
+     ""DepartureLocation"": ""55/015/0071"",
+     ""DepartureCoreID"": ""98005647"",
+     ""DepartureLocationType"": ""Agricultural Holding"",
+     ""DeparturePremisesActivity"": ""AH"",
+     ""OffExemptionCode"": """",
+     ""ArrivalDate"": ""2026-02-26"",
+     ""DestinationLocation"": ""55/014/0006"",
+     ""DestinationCoreID"": ""98045305"",
+     ""DestinationLocationType"": ""Agricultural Holding"",
+     ""DestinationPremisesActivity"": ""AH"",
+     ""OnExemptionCode"": """",
+     ""LotNumber"": """",
+     ""NumberOfAnimals"": 3,
+     ""NumberOfDOAs"": 0,
+     ""NumberOfReads"": 3,
+     ""LoadingTimestamp"": """",
+     ""ExpectedJourneyDuration"": """",
+     ""UnloadingTimestamp"": """",
+     ""AnimalTransporter"": ""01"",
+     ""HaulierDetails"": {
+       ""Name"": """",
+       ""Telephone"": """",
+       ""Email"": """",
+       ""VehicleDetails"": [
+         {
+           ""VehicleRegistrationNumber"": """",
+           ""DriverName"": """",
+           ""TransportAuthorisationNumber"": """"
+         }
+       ]
+     }
+   },
+   ""AnimalGroupDetails"": [
+   ],
+   ""AnimalDetails"": [
+     {
+       ""AnimalID"": ""826074973200188"",
+       ""AnimalIDType"": ""IM"",
+       ""AnimalIDTransponderHex"": """",
+       ""AnimalIDTransponderISO"": """",
+       ""TagIssueNumber"": """",
+       ""DOA"": false
+     },
+     {
+       ""AnimalID"": ""826074191409879"",
+       ""AnimalIDType"": ""IM"",
+       ""AnimalIDTransponderHex"": """",
+       ""AnimalIDTransponderISO"": """",
+       ""TagIssueNumber"": """",
+       ""DOA"": false
+     },
+     {
+       ""AnimalID"": ""826074973200206"",
+       ""AnimalIDType"": ""IM"",
+       ""AnimalIDTransponderHex"": """",
+       ""AnimalIDTransponderISO"": """",
+       ""TagIssueNumber"": """",
+       ""DOA"": false
+     }
+   ]
+ }
+}";
 }
