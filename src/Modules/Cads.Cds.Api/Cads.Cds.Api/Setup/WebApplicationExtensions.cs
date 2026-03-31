a8bff0671c18b5b59a6481852a46dd819b7e7cbd
andy-defra <andy.rose@esynergy.co.uk>
2026-03-31 17:37:55 +0100

    Adding conditional support for Https

--- src/Modules/Cads.Cds.Api/Cads.Cds.Api/Setup/WebApplicationExtensions.cs
+++ src/Modules/Cads.Cds.Api/Cads.Cds.Api/Setup/WebApplicationExtensions.cs
@@ -3,6 +3,7 @@ using Cads.Cds.Api.Application.Soap.ServiceContracts.Abstractions;
 using CoreWCF.Channels;
 using CoreWCF.Configuration;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 
 namespace Cads.Cds.Api.Setup;
 
@@ -17,24 +18,39 @@ public static class WebApplicationExtensions
 
     public static IApplicationBuilder UseApiSoapEndpoints(this IApplicationBuilder app)
     {
+        var hasHttps = HasHttpsAddress(app);
         app.UseServiceModel(builder =>
         {
             builder.AddServiceEndpoints<IAnimalCohortServiceContract, AnimalCohortServiceContract>(
-                    "/api/soap/AnimalCohorts.asmx")
+                    "/api/soap/AnimalCohorts.asmx", hasHttps)
                 .AddServiceEndpoints<IAnimalDetailsServiceContract, AnimalDetailsServiceContract>(
-                    "/api/soap/AnimalDetails.asmx")
+                    "/api/soap/AnimalDetails.asmx", hasHttps)
                 .AddServiceEndpoints<IAnimalPassportAndDetailsServiceContract, AnimalPassportAndDetailsServiceContract>(
-                    "/api/soap/AnimalPassportAndDetails.asmx")
+                    "/api/soap/AnimalPassportAndDetails.asmx", hasHttps)
                 .AddServiceEndpoints<ICattleStatusServiceContract, CattleStatusServiceContract>(
-                    "/api/soap/CattleStatus.asmx")
+                    "/api/soap/CattleStatus.asmx", hasHttps)
                 .AddServiceEndpoints<ILivestockMovementsServiceContract, LivestockMovementsServiceContract>(
-                    "/api/soap/LivestockMovements.asmx");
+                    "/api/soap/LivestockMovements.asmx", hasHttps);
         });
 
         return app;
     }
 
-    private static IServiceBuilder AddServiceEndpoints<TServiceContract, TServiceImplementation>(this IServiceBuilder builder, string path)
+    private static bool HasHttpsAddress(IApplicationBuilder app)
+    {
+        try
+        {
+            var serverAddresses = app.ServerFeatures.Get<IServerAddressesFeature>();
+            return serverAddresses?.Addresses.Any(a =>
+                a.StartsWith("https", StringComparison.OrdinalIgnoreCase)) == true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private static IServiceBuilder AddServiceEndpoints<TServiceContract, TServiceImplementation>(this IServiceBuilder builder, string path, bool includeHttps)
         where TServiceContract : class
         where TServiceImplementation : class, TServiceContract
     {
@@ -42,9 +58,13 @@ public static class WebApplicationExtensions
         builder.AddServiceEndpoint<TServiceImplementation, TServiceContract>(
             _soap12BindingHttp,
             path);
-        builder.AddServiceEndpoint<TServiceImplementation, TServiceContract>(
-            _soap12BindingHttps,
-            path);
+        
+        if (includeHttps)
+        {
+            builder.AddServiceEndpoint<TServiceImplementation, TServiceContract>(
+                _soap12BindingHttps,
+                path);
+        }
 
         return builder;
     }
