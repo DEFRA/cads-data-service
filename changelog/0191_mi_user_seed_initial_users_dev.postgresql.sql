dd1f7c8160822305ad05694bf55a6d4200b90602
Mark Gent <Mark.Gent@homesengland.gov.uk>
2026-04-08 12:00:56 +0100

    ULITP-4787: Updated user seed files to use props

--- changelog/0191_mi_user_seed_initial_users_dev.postgresql.sql
+++ changelog/0191_mi_user_seed_initial_users_dev.postgresql.sql
@@ -3,80 +3,80 @@
 --changeset mark:cleanup-gary-fletcher-roles context:dev
 DELETE FROM mi_user_role
 WHERE user_id IN (
-    SELECT user_id FROM mi_user WHERE email = 'gary.fletcher@defra.gov.uk'
+    SELECT user_id FROM mi_user WHERE email = '${gfEmail}'
 );
 
 --changeset mark:cleanup-gary-fletcher-user context:dev
 DELETE FROM mi_user
-WHERE email = 'gary.fletcher@defra.gov.uk';
+WHERE email = '${gfEmail}';
 
 
 --changeset mark:seed-mark-gent context:dev
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'mark.gent@defra.gov.uk', 'Mark Gent', 'mark.gent@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${mgEmail}', '${mgName}', '${mgEmail}', true);
 
 --changeset mark:seed-mark-gent-role context:dev
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'mark.gent@defra.gov.uk';
+WHERE u.email = '${mgEmail}';
 
 --changeset mark:seed-gary-fletcher context:dev
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'gary.fletcher@defra.gov.uk', 'Gary Fletcher', 'gary.fletcher@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${gfEmail}', '${gfName}', '${gfEmail}', true);
 
 --changeset mark:seed-gary-fletcher-role context:dev
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'gary.fletcher@defra.gov.uk';
+WHERE u.email = '${gfEmail}';
 
 
 --changeset mark:seed-craig-john context:dev
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'craig.john@defra.gov.uk', 'Craig John', 'craig.john@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${cjEmail}', '${cjName}', '${cjEmail}', true);
 
 --changeset mark:seed-craig-john-role context:dev
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'craig.john@defra.gov.uk';
+WHERE u.email = '${cjEmail}';
 
 
 --changeset mark:seed-david-baillie context:dev
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'David.Baillie@defra.gov.uk', 'David Baillie', 'David.Baillie@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${dbEmail}', '${dbName}', '${dbEmail}', true);
 
 --changeset mark:seed-david-baillie-role context:dev
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'David.Baillie@defra.gov.uk';
+WHERE u.email = '${dbEmail}';
 
 
 --changeset mark:seed-andy-rose context:dev
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'Andy.Rose@defra.gov.uk', 'Andy Rose', 'Andy.Rose@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${arEmail}', '${arName}', '${arEmail}', true);
 
 --changeset mark:seed-andy-rose-role context:dev
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'Andy.Rose@defra.gov.uk';
+WHERE u.email = '${arEmail}';
 
 
 --changeset mark:seed-tom-harrison context:dev
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'Tom.Harrison@defra.gov.uk', 'Tom Harrison', 'Tom.Harrison@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${thEmail}', '${thName}', '${thEmail}', true);
 
 --changeset mark:seed-tom-harrison-role context:dev
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'Tom.Harrison@defra.gov.uk';
+WHERE u.email = '${thEmail}';
--- changelog/0194_mi_user_seed_initial_users_perf-test.postgresql.sql
+++ changelog/0194_mi_user_seed_initial_users_perf-test.postgresql.sql
@@ -2,70 +2,70 @@
 
 --changeset mark:seed-mark-gent context:perf-test
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'mark.gent@defra.gov.uk', 'Mark Gent', 'mark.gent@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${mgEmail}', '${mgName}', '${mgEmail}', true);
 
 --changeset mark:seed-mark-gent-role context:perf-test
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'mark.gent@defra.gov.uk';
+WHERE u.email = '${mgEmail}';
 
 --changeset mark:seed-gary-fletcher context:perf-test
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'gary.fletcher@defra.gov.uk', 'Gary Fletcher', 'gary.fletcher@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${gfEmail}', '${gfName}', '${gfEmail}', true);
 
 --changeset mark:seed-gary-fletcher-role context:perf-test
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'gary.fletcher@defra.gov.uk';
+WHERE u.email = '${gfEmail}';
 
 
 --changeset mark:seed-craig-john context:perf-test
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'craig.john@defra.gov.uk', 'Craig John', 'craig.john@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${cjEmail}', '${cjName}', '${cjEmail}', true);
 
 --changeset mark:seed-craig-john-role context:perf-test
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'craig.john@defra.gov.uk';
+WHERE u.email = '${cjEmail}';
 
 
 --changeset mark:seed-david-baillie context:perf-test
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'David.Baillie@defra.gov.uk', 'David Baillie', 'David.Baillie@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${dbEmail}', '${dbName}', '${dbEmail}', true);
 
 --changeset mark:seed-david-baillie-role context:perf-test
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'David.Baillie@defra.gov.uk';
+WHERE u.email = '${dbEmail}';
 
 
 --changeset mark:seed-andy-rose context:perf-test
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'Andy.Rose@defra.gov.uk', 'Andy Rose', 'Andy.Rose@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${arEmail}', '${arName}', '${arEmail}', true);
 
 --changeset mark:seed-andy-rose-role context:perf-test
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'Andy.Rose@defra.gov.uk';
+WHERE u.email = '${arEmail}';
 
 
 --changeset mark:seed-tom-harrison context:perf-test
 INSERT INTO mi_user (user_id, external_subject, display_name, email, is_active)
-VALUES (gen_random_uuid(), 'Tom.Harrison@defra.gov.uk', 'Tom Harrison', 'Tom.Harrison@defra.gov.uk', true);
+VALUES (gen_random_uuid(), '${thEmail}', '${thName}', '${thEmail}', true);
 
 --changeset mark:seed-tom-harrison-role context:perf-test
 INSERT INTO mi_user_role (user_id, role_id)
 SELECT u.user_id, r.role_id
 FROM mi_user u
 JOIN mi_role r ON r.role_key = 'MI_ADMIN'
-WHERE u.email = 'Tom.Harrison@defra.gov.uk';
+WHERE u.email = '${thEmail}';
