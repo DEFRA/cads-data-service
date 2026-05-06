02a4b5e40878a9633f2ff7278f289822b5a4ac93
andy-defra <andy.rose@esynergy.co.uk>
2026-05-06 10:10:32 +0100

    Fixing broken view creation

--- changelog/0001_000__create_mi_tables.postgresql.sql
+++ changelog/0001_000__create_mi_tables.postgresql.sql
@@ -82,34 +82,22 @@ ALTER TABLE "mi_user_report_permission" ADD CONSTRAINT "mi_user_report_permissio
 
 DROP VIEW IF EXISTS "mi_effective_report_permission";
 
-CREATE OR REPLACE VIEW "mi_effective_report_permission" AS
-SELECT
-    u.display_name,
-    u.email,
+CREATE OR REPLACE VIEW "mi_effective_report_permission" AS SELECT rep.report_id,
     rep.report_key,
     rep.title,
     rep.description,
-    COALESCE(upr.granted, bool_or(rpr.granted), false) AS granted
-FROM mi_user u
-JOIN mi_report rep ON true
-JOIN mi_permission p ON p.permission_key = 'REPORT_VIEW'
-LEFT JOIN mi_user_report_permission upr
-    ON upr.user_id = u.user_id
-   AND upr.report_id = rep.report_id
-   AND upr.permission_id = p.permission_id
-LEFT JOIN mi_user_role ur
-    ON ur.user_id = u.user_id
-LEFT JOIN mi_role_report_permission rpr
-    ON rpr.role_id = ur.role_id
-   AND rpr.report_id = rep.report_id
-   AND rpr.permission_id = p.permission_id
-GROUP BY
+    COALESCE(rep.is_active, false) AS is_active,
     u.display_name,
-    u.email,
-    rep.report_id,
-    p.permission_id,
-    upr.granted
-HAVING COALESCE(upr.granted, bool_or(rpr.granted), false);
+    COALESCE(u.external_subject_normalized, u.external_subject) AS external_subject,
+    COALESCE(upr.granted, bool_or(rpr.granted), false) AS granted
+   FROM (((((mi_user u
+     JOIN mi_report rep ON (true))
+     JOIN mi_permission p ON ((p.permission_key = 'REPORT_VIEW'::text)))
+     LEFT JOIN mi_user_report_permission upr ON (((upr.user_id = u.user_id) AND (upr.report_id = rep.report_id) AND (upr.permission_id = p.permission_id))))
+     LEFT JOIN mi_user_role ur ON ((ur.user_id = u.user_id)))
+     LEFT JOIN mi_role_report_permission rpr ON (((rpr.role_id = ur.role_id) AND (rpr.report_id = rep.report_id) AND (rpr.permission_id = p.permission_id))))
+  GROUP BY u.display_name, u.external_subject, u.external_subject_normalized, rep.report_key, rep.report_id, p.permission_id, upr.granted
+ HAVING (COALESCE(upr.granted, bool_or(rpr.granted), false) = true);
 
 DROP VIEW IF EXISTS mi_effective_report_all_permission;
 
