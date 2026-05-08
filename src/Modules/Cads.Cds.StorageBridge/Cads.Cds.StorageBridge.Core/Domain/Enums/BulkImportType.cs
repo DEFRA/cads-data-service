0edc566bb0dcfe51af2a4d370f055c855bb7b2ae
David Baillie <david.baillie@esynergy.co.uk>
2026-05-08 15:03:41 +0100

    Added primary keys to BulkImportType enumerations.

--- src/Modules/Cads.Cds.StorageBridge/Cads.Cds.StorageBridge.Core/Domain/Enums/BulkImportType.cs
+++ src/Modules/Cads.Cds.StorageBridge/Cads.Cds.StorageBridge.Core/Domain/Enums/BulkImportType.cs
@@ -5,23 +5,23 @@ namespace Cads.Cds.StorageBridge.Core.Domain.Enums;
 public enum BulkImportType
 {
     None,
-    [TableName("_ct_animal_relationships")]
+    [TableName("_ct_animal_relationships", "aar_id")]
     AnimalRelationships,
-    [TableName("_ct_animal_statuses")]
+    [TableName("_ct_animal_statuses", "ast_id")]
     AnimalStatuses,
-    [TableName("_ct_condition_markers")]
+    [TableName("_ct_condition_markers", "com_id")]
     ConditionMarkers,
-    [TableName("_ct_eartags")]
+    [TableName("_ct_eartags", "etg_id")]
     EarTags,
-    [TableName("_ct_issued_documents")]
+    [TableName("_ct_issued_documents","ido_id")]
     IssuedDocuments,
-    [TableName("_ct_parties")]
+    [TableName("_ct_parties", "par_id")]
     Parties,
-    [TableName("_ct_registered_animals")]
+    [TableName("_ct_registered_animals","ran_id")]
     RegisteredAnimals,
-    [TableName("_ct_registered_movements")]
+    [TableName("_ct_registered_movements", "mov_id")]
     RegisteredMovements,
-    [TableName("_ct_valid_applications")]
+    [TableName("_ct_valid_applications", "vap_id")]
     ValidApplications,
     [TableName("_ct_locations", "loc_id")]
     Locations
