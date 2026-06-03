dd0d2afacde41883df22ae8ff97984cc855e2200
Mark Gent <Mark.Gent@homesengland.gov.uk>
2026-06-03 13:11:49 +0100

    ULITP-5372: Commit 2 needed due to publish bug.

--- src/Modules/Cads.Cds.Api/Cads.Cds.Api.Application/Queries/Locations/LocationsQueryAdapter.cs
+++ src/Modules/Cads.Cds.Api/Cads.Cds.Api.Application/Queries/Locations/LocationsQueryAdapter.cs
@@ -4,6 +4,12 @@ namespace Cads.Cds.Api.Application.Queries.Locations;
 
 public class LocationsQueryAdapter
 {
+    /// <summary>
+    /// No data layer yet so just returns empty list.
+    /// </summary>
+    /// <param name="query"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<(List<LocationDto> Items, int TotalCount)> GetLocationsAsync(
         GetLocationsQuery query,
         CancellationToken cancellationToken = default)
