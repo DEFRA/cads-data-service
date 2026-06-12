30ca83a587c3565cbf54d6bca1babd569e08ab72
Tom Harrison <taharrison@gmail.com>
2026-06-12 12:05:00 +0100

    Add validator for CPH format

--- src/Modules/Cads.Cds.Api/Cads.Cds.Api.Application/Queries/Locations/GetLocationsQuery.cs
+++ src/Modules/Cads.Cds.Api/Cads.Cds.Api.Application/Queries/Locations/GetLocationsQuery.cs
@@ -5,6 +5,10 @@ namespace Cads.Cds.Api.Application.Queries.Locations;
 
 public class GetLocationsQuery : IQuery<IEnumerable<LocationDto>>
 {
+    /// <summary>
+    /// CPH property is the LID_FULL_IDENTIFIER which is a case sensitive string in this format: AA-12/345/6789
+    /// ie. the Cph with a prefix of the location type
+    /// </summary>
     public string? Cph { get; set; }
     public DateOnly? LastModifiedDate { get; set; }
 }
