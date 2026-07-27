ed16c1c3121adf5489684e3b3995c49134a3e16f
Mark Gent <Mark.Gent@homesengland.gov.uk>
2026-07-27 15:40:04 +0100

    LANI-729: Set destination table name as lower case

--- src/BuildingBlocks/Cads.Cds.BuildingBlocks.Application/Schema/SchemaHelper.cs
+++ src/BuildingBlocks/Cads.Cds.BuildingBlocks.Application/Schema/SchemaHelper.cs
@@ -22,6 +22,6 @@ public static class SchemaHelper
             throw new UnprocessableException($"Invalid import action type '{parsedFilename!.Type}' derived from file name '{parsedFilename!.TableName}'.");
         }
 
-        return $"{schemaName.GetDescription()}.{parsedFilename!.TableName}";
+        return $"{schemaName.GetDescription()}.{parsedFilename!.TableName.ToLower()}";
     }
 }
