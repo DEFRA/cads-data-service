174c81fdf47341143f48c5274f9015afeb133bf9
andy-defra <andy.rose@esynergy.co.uk>
2026-08-20 15:46:20 +0100

    format

--- tests/BuildingBlocks/Cads.Cds.BuildingBlocks.Core.Tests.Unit/Domain/Imports/FileImportTests.cs
+++ tests/BuildingBlocks/Cads.Cds.BuildingBlocks.Core.Tests.Unit/Domain/Imports/FileImportTests.cs
@@ -40,4 +40,3 @@ public class FileImportTests
         fileImport.ImportStatus.Should().Be(FileImportStatus.Failed);
     }
 }
-
--- tests/Modules/Cads.Cds.SystemAdmin/Cads.Cds.SystemAdmin.Application.Tests.Unit/TestFixtures/CreateFileImportCommandHandlerTests.cs
+++ tests/Modules/Cads.Cds.SystemAdmin/Cads.Cds.SystemAdmin.Application.Tests.Unit/TestFixtures/CreateFileImportCommandHandlerTests.cs
@@ -75,5 +75,3 @@ public class CreateFileImportCommandHandlerTests
             Times.Once);
     }
 }
-
-
