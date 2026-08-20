beabd14b0fcdd40da77e2af66f038bc749d46782
James Grafton <james.grafton@stillsoftware.com>
2026-08-20 15:48:55 +0100

    Extracts class constant into a shared class.

--- tests/Modules/Cads.Cds.StorageBridge/Cads.Cds.StorageBridge.Tests.Component/TestFixtures/StorageManagementTestFixture.cs
+++ tests/Modules/Cads.Cds.StorageBridge/Cads.Cds.StorageBridge.Tests.Component/TestFixtures/StorageManagementTestFixture.cs
@@ -1,10 +1,10 @@
 using Cads.Cds.BuildingBlocks.Testing.Support.TestFixtures.Components;
+using Cads.Cds.StorageBridge.Testing.Support.Constants;
 
 namespace Cads.Cds.StorageBridge.Tests.Component.TestFixtures;
 
 public class StorageManagementTestFixture : TestFixtureBase<Program, StorageBridgeWebApplicationFactory>
 {
-    public const string Salt = "test-salt";
 
     public StorageManagementTestFixture()
         : base(CreateFactory())
@@ -14,7 +14,7 @@ public class StorageManagementTestFixture : TestFixtureBase<Program, StorageBrid
     private static StorageBridgeWebApplicationFactory CreateFactory()
     {
         Environment.SetEnvironmentVariable("Modules__StorageBridge__Storage__StorageManager__Enabled", "true");
-        Environment.SetEnvironmentVariable("Modules__StorageBridge__Storage__StorageManager__Salt", Salt);
+        Environment.SetEnvironmentVariable("Modules__StorageBridge__Storage__StorageManager__Salt", S3StorageConstants.Salt);
 
         return new StorageBridgeWebApplicationFactory(useFakeAuth: true);
     }
