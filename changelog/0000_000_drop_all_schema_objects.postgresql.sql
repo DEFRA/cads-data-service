c6095998845f305b2e73951cb79d8cbe98a25799
Mark Gent <MarkGent1@users.noreply.github.com>
2026-09-18 15:49:54 +0100

    Feature/lani 931 lb script (#166)
    * LANI-931: Added updated parallel bulk import
    
    * LANI-931: Added cron worker close down
    
    ---------
    
    Co-authored-by: Mark Gent <Mark.Gent@homesengland.gov.uk>

--- changelog/0000_000_drop_all_schema_objects.postgresql.sql
+++ changelog/0000_000_drop_all_schema_objects.postgresql.sql
@@ -4,6 +4,22 @@
 -- DELETE FROM databasechangeloglock;
 -- DELETE FROM databasechangelog;
 
+-- Remove old run-1 schedules at changeset application time. This is deliberately
+-- outside all procedures and only targets this import's own job-name prefix.
+DO $cron_cleanup$
+DECLARE
+    v_jobid bigint;
+BEGIN
+    IF to_regclass('cron.job') IS NOT NULL THEN
+        FOR v_jobid IN EXECUTE
+            'SELECT jobid FROM cron.job WHERE jobname LIKE ''cts-import-run-1-worker-%'''
+        LOOP
+            EXECUTE 'SELECT cron.unschedule($1)' USING v_jobid;
+        END LOOP;
+    END IF;
+END;
+$cron_cleanup$;
+
 -- changeset schema:0000-000-drop-all-schema-1 objects splitStatements:false
 DROP SCHEMA IF EXISTS cads CASCADE;
 
