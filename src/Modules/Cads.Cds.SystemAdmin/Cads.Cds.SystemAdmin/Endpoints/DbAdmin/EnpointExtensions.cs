1a15693397bc99aafed19687f6737a2b16f3c6e9
andy-defra <andy.rose@esynergy.co.uk>
2026-09-25 14:06:04 +0100

    Additional test coverage for DbAdminExecuteCommandService

--- src/Modules/Cads.Cds.SystemAdmin/Cads.Cds.SystemAdmin/Endpoints/DbAdmin/EnpointExtensions.cs
+++ src/Modules/Cads.Cds.SystemAdmin/Cads.Cds.SystemAdmin/Endpoints/DbAdmin/EnpointExtensions.cs
@@ -1,10 +1,14 @@
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
 using Cads.Cds.BuildingBlocks.Infrastructure.Authentication.Configuration;
 using Cads.Cds.SystemAdmin.Application.DbAdmin.Services;
 using Cads.Cds.SystemAdmin.Endpoints.DbAdmin.Requests;
 using Cads.Cds.SystemAdmin.Endpoints.DbAdmin.Responses;
 using FluentValidation;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
 
 namespace Cads.Cds.SystemAdmin.Endpoints.DbAdmin;
 
@@ -19,10 +23,16 @@ public static class EnpointExtensions
     private static async Task<DbAdminExecuteCommandResponse> DbAdminExecuteCommand(
         DbAdminExecuteCommandRequest request,
         IValidator<DbAdminExecuteCommandRequest> validator,
-        IDbAdminExecuteCommandService service)
+        IDbAdminExecuteCommandService service,
+        HttpContext httpContext,
+        ILogger<DbAdminExecuteCommandRequest> logger)
     {
         await validator.ValidateAndThrowAsync(request);
-
+        if (logger.IsEnabled(LogLevel.Information))
+        {
+            var user = httpContext.User.Identity!.Name;
+            logger.LogInformation("User {User}: Executing DB Admin command: {Command} with args: {Args}", user, request.Command, request.Args);
+        }
         var result = await service.ExecuteAsync(request.Command, request.Args);
 
         return new DbAdminExecuteCommandResponse(
